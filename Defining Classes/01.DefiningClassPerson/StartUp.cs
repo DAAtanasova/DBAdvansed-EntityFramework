6126e6fe57b21a91944d72eb6322968c86ad5d7c
DAAtanasova <mad_n_happyy@abv.bg>
2017-10-29 19:10:34 +0200

    Add files via upload

---
+++ Defining Classes/01.DefiningClassPerson/StartUp.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Reflection;
+
+class StartUp
+    {
+        public static void Main(string[] args)
+        {
+        var person = new Person()
+        {
+            Name = "Pesho",
+            Age = 25
+        };
+
+        //Required for Judge; Given from SoftUni
+        //Type personType = typeof(Person);
+        //PropertyInfo[] properties = personType.GetProperties
+        //    (BindingFlags.Public | BindingFlags.Instance);
+        //Console.WriteLine(properties.Length);
+
+        Console.WriteLine($"{person.Name} is {person.Age} years old.");
+
+        }
+
+    }
+
