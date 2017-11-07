85397abd2163e3674da66d21c2ab15732ef29bf0
D.Atanasova <mad_n_happyy@abv.bg>
2017-11-07 19:18:52 +0200

    Update Program.cs

--- ADO.NET/07.PrintAllMinions/Program.cs
+++ ADO.NET/07.PrintAllMinions/Program.cs
@@ -6,7 +6,7 @@ public class Program
 {
     static void Main(string[] args)
     {
-        var connection = new SqlConnection("Server=;Database=minionsDB;Integrated Security ");
+        var connection = new SqlConnection("");
         connection.Open();
 
         using (connection)
