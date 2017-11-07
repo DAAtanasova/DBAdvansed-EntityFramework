bf1ead39fa34b9a2b5febe578f2c6abd01ca40cc
D.Atanasova <mad_n_happyy@abv.bg>
2017-11-07 16:00:54 +0200

    Add files via upload

---
+++ ADO.NET/07.PrintAllMinions/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        var connection = new SqlConnection("Server=.;Database=minionsDB;Integrated Security = true");
+        connection.Open();
+
+        using (connection)
+        {
+            try
+            {
+                var command = new SqlCommand("SELECT Name FROM Minions", connection);
+                var reader = command.ExecuteReader();
+                var minions = new List<string>();
+                using (reader)
+                {
+                    try
+                    {
+                        while (reader.Read())
+                        {
+                            string minion = (string)reader[0];
+                            minions.Add(minion);
+                        }
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine(e.Message);
+                    }
+                }
+
+                for (int i = 0; i < minions.Count / 2; i++)
+                {
+                    Console.WriteLine(minions[i]);
+                    Console.WriteLine(minions[minions.Count - 1 - i]);
+                }
+                if (minions.Count % 2 == 1)
+                {
+                    Console.WriteLine(minions[minions.Count / 2]);
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
